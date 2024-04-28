ebe93740f3dd3bf46105b6636cfc54013eb432cd
WJYEE <100765886+WJYEE@users.noreply.github.com>
2024-04-28 13:12:58 +0900

    Title: 대문자로 바꾸기, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240428/대문자로 바꾸기/change-to-capital.py
+++ 240428/대문자로 바꾸기/change-to-capital.py
@@ -1,3 +1,6 @@
 arr = [list(map(str,input().split())) for _ in range(5)]
-arr = arr.upper()
-print(arr)
+for i in range(5):
+    for j in range(3):
+        arr[i][j] = arr[i][j].upper()
+        print(arr[i][j], end=' ')
+    print()
