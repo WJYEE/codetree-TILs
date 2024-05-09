a0d550309109520cbb34d0d1559ed7fbfa73d2ea
WJYEE <100765886+WJYEE@users.noreply.github.com>
2024-05-09 21:26:35 +0900

    Title: 숫자 직사각형, Time: 136ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240509/숫자 직사각형/rectangle-of-numbers.py
@@ -0,0 +1,16 @@
+n,m = map(int,input().split())
+arr = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
+num = 1
+
+for i in range(n):
+    for j in range(m):
+        arr[i][j]=num
+        num+=1
+
+for i in range(n):
+    for j in range(m):
+        print(arr[i][j],end=' ')
+    print()
