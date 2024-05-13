557087a476f18f1b8209ecc320ebcf8f8d6bd7fa
WJYEE <100765886+WJYEE@users.noreply.github.com>
2024-05-14 00:35:50 +0900

    Title: 되돌아오기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240514/되돌아오기 2/come-back-2.py
@@ -0,0 +1,17 @@
+x,y = 0
+string = input()
+N = len(string)
+time = 0
+dx,dy = [1,0,-1,0],[0,1,0,-1]
+direct = 3
+for i in range(N):
+    if string[i]=='L':
+        direct = (direct+3)%4
+    elif string[i]=='R':
+        direct = (direct+1)%4
+    else:
+        x,y = x+dx[direct],y+dy[direct]
+    time+=1
+    if x==0 and y==0:
+        break
+print(time)
