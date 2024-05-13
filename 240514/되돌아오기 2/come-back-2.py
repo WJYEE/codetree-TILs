1a9ee3dc9c2c297788d9d0b0442ad18d98ad5a83
WJYEE <100765886+WJYEE@users.noreply.github.com>
2024-05-14 00:38:31 +0900

    Title: 되돌아오기 2, Time: 222ms, Memory: 25MB, Status: Passed - CodeTree

--- 240514/되돌아오기 2/come-back-2.py
+++ 240514/되돌아오기 2/come-back-2.py
@@ -4,6 +4,7 @@ N = len(string)
 time = 0
 dx,dy = [1,0,-1,0],[0,1,0,-1]
 direct = 3
+answer=0
 for i in range(N):
     if string[i]=='L':
         direct = (direct+3)%4
@@ -13,8 +14,9 @@ for i in range(N):
         x,y = x+dx[direct],y+dy[direct]
     time+=1
     if x==0 and y==0:
+        answer=1
         break
-if time > N:
+if answer == 0:
     print(-1)
 else:
     print(time)
