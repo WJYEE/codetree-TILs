9342dc6f074302ae56b5decf82c3e954b2488e21
WJYEE <100765886+WJYEE@users.noreply.github.com>
2024-05-15 20:11:55 +0900

    Title: 최고의 33위치, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240515/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,13 @@
+N=int(input())
+grid = [list(map(int,input().split())) for _ in range(N)]
+cnt=0
+m_cnt =0
+for y in range(N-2):
+    for x in range(N-2):
+        for j in range(y,3+y):
+            for i in range(x,3+x):
+                cnt+=grid[j][i]
+        if cnt>m_cnt:
+            m_cnt=cnt
+
+print(m_cnt)
